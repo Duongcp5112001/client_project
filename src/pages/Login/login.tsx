868d997006a7c0da16f6ec8190d13a25bac10da2
Duongcp5112001 <77434155+Duongcp5112001@users.noreply.github.com>
2023-10-16 14:38:29 +0700

    16-10

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,19 +1,42 @@
 import React from 'react'; 
+import { FunctionComponent } from "react";
+import "./style.css";
+import Subtract from "../../assets/login/subtract.svg"
+import Frame from "../../assets/login/frame.svg"
+import Frame1 from "../../assets/login/frame1.svg"
   
-const Login = () => { 
+const Login: FunctionComponent = () => { 
   return ( 
-    <div 
-      style={{ 
-        display: 'flex', 
-        justifyContent: 'Center', 
-        alignItems: 'Right', 
-        width: '80%',
-        marginLeft: '20%',
-        margin: '0 auto',
-      }} 
-    > 
-      <h1>Login da sua?</h1> 
-    </div> 
+    <div className="minimalism-login-page">
+      <img
+        className="minimalism-login-page-child"
+        alt=""
+        src="/rectangle-4@2x.png"
+      />
+      <div className="welcome-parent">
+        <b className="welcome">Welcome</b>
+      </div>
+      <div className="login-with-others-parent">
+        <div className="login-with-others-container">
+          <a>Forgot password?</a>
+        </div>
+        <img className="subtract-icon" alt="" src={Subtract} />
+      </div>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="password">Password</div>
+        <img className="frame-icon" alt="" src={Frame} />
+      </div>
+      <div className="rectangle-group">
+        <div className="group-child" />
+        <div className="password">Username</div>
+        <img className="frame-icon" alt="" src={Frame1} />
+      </div>
+      <div className="rectangle-container">
+        <div className="group-inner" />
+        <b className="next">LOGIN</b>
+      </div>
+    </div>
   ); 
 }; 
 
