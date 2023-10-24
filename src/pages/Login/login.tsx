984dfc6708522c035eee7fd0c1a604033aae1f88
beast30122001 <77433462+beast30122001@users.noreply.github.com>
2023-10-24 22:29:13 +0700

    123

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,44 +1,78 @@
-import { FunctionComponent } from "react";
-import { Navigate } from "react-router-dom";
-import "./style.css";
-import Subtract from "../../assets/login/subtract.svg"
-import Frame from "../../assets/login/frame.svg"
-import Frame1 from "../../assets/login/frame1.svg"
-import Rectangle from "../../assets/login/rectangle-4@2x.png"
-  
-const Login: FunctionComponent = () => { 
-  return ( 
-    <div className="minimalism-login-page">
-      <img
-        className="minimalism-login-page-child"
-        alt=""
-        src={Rectangle}
-      />
-      <div className="welcome-parent">
-        <b className="welcome">Welcome</b>
-      </div>
-      <div className="login-with-others-parent">
-        <div className="login-with-others-container">
-          <div>Forgot password?</div>
-        </div>
-        <img className="subtract-icon" alt="" src={Subtract} />
-      </div>
-      <div className="rectangle-parent">
-        <div className="group-child" />
-        <div className="password">Password</div>
-        <img className="frame-icon" alt="" src={Frame} />
-      </div>
-      <div className="rectangle-group">
-        <div className="group-child" />
-        <div className="password">Username</div>
-        <img className="frame-icon" alt="" src={Frame1} />
-      </div>
-      <div className="rectangle-container">
-        <div className="group-inner" />
-        <b className="next">LOGIN</b>
-      </div>
-    </div>
-  ); 
-}; 
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import {
+  Form,
+  Input,
+} from 'antd';
 
+interface AppState {
+  login: string;
+}
+  
+class Login extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { login: 'show col-lg-6 px-lg-4' };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="page-holder align-items-center py-4 bg-gray-100 vh-100">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className={this.state.login}>
+                <div className="card">
+                  <div className="card-body p-lg-5">
+                    <h3 className="mb-4">Login</h3>
+                    <form action="index.html">
+                      <div className="form-floating mb-3">
+                        <input className="form-control" id="floatingInput" type="email" placeholder="name@example.com" required />
+                        <label htmlFor="floatingInput">Email address</label>
+                        <div className="valid-feedback">Valid.</div>
+                        <div className="invalid-feedback">Please fill out this field.</div>
+                      </div>
+                      <div className="form-floating mb-3">
+                        <input className="form-control" id="floatingPassword" type="password" placeholder="Password" required />
+                        <label htmlFor="floatingPassword">Password</label>
+                        <div className="valid-feedback">Valid.</div>
+                        <div className="invalid-feedback">Please fill out this field.</div>
+                      </div>
+                        <a href="#">Forget Password</a>
+                      <div className="form-group">
+                        <button className="btn btn-primary" id="login" type="button" name="loginSubmit" >
+                          Login
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="card-footer px-lg-5 py-lg-4">
+                    <div className="text-sm text-muted">
+                      First time ? <a>Register</a>.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 col-xl-5 ms-xl-auto px-lg-4 text-center text-primary">
+                <img
+                  className="img-fluid mb-4"
+                  width="300"
+                  src="https://therichpost.com/wp-content/uploads/2021/06/login_page_image.png"
+                  alt=""
+                  style={{ transform: "rotate(10deg)" }}
+                />
+                <h1 className="mb-4">
+                </h1>
+                <p className="lead text-muted">
+                  Welcome to the world of endless possibilities and opportunities
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default Login;
